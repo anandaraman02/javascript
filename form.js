cc7f71a13c68d2de26eae24422d29b511d1e3dff
anandaraman02 <anandaraman.997@gmail.com>
2023-10-18 18:17:21 +0530

    javascript

---
+++ form.js
@@ -0,0 +1,93 @@
+const form = document.querySelector('#form')
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const cpassword = document.querySelector('#cpassword');
+
+form.addEventListener('submit',(e)=>{
+    
+    if(!validateinput()){
+        e.preventDefault();
+    }
+})
+
+function validateinput(){
+    const usernameVal = username.value.trim()
+    const emailVal = email.value.trim();
+    const passwordVal = password.value.trim();
+    const cpasswordVal = cpassword.value.trim();
+    let success = true
+
+    if(usernameVal===''){
+        success=false;
+        setError(username,'Username is required')
+    }
+    else{
+        setSuccess(username)
+    }
+
+    if(emailVal===''){
+        success = false;
+        setError(email,'Email is required')
+    }
+    else if(!validateEmail(emailVal)){
+        success = false;
+        setError(email,'Please enter a valid email')
+    }
+    else{
+        setSuccess(email)
+    }
+
+    if(passwordVal === ''){
+        success= false;
+        setError(password,'Password is required')
+    }
+    else if(passwordVal.length<6){
+        success = false;
+        setError(password,'Password must be atleast 6 characters long')
+    }
+    else{
+        setSuccess(password)
+    }
+
+    if(cpasswordVal === ''){
+        success = false;
+        setError(cpassword,'Confirm password is required')
+    }
+    else if(cpasswordVal!==passwordVal){
+        success = false;
+        setError(cpassword,'Password does not match')
+    }
+    else{
+        setSuccess(cpassword)
+    }
+
+    return success;
+
+}
+//element - password, msg- pwd is reqd
+function setError(element,message){
+    const inputgroup = element.parentElement;
+    const errorElement = inputgroup.querySelector('.error-message')
+
+    errorElement.innerText = message;
+    inputgroup.classList.add('error')
+    inputgroup.classList.remove('success')
+}
+
+function setSuccess(element){
+    const inputgroup = element.parentElement;
+    const errorElement = inputgroup.querySelector('.error-message')
+
+    errorElement.innerText = '';
+    inputgroup.classList.add('success')
+    inputgroup.classList.remove('error')
+}
+
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
